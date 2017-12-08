52b3956ab182de76a9effb128ecfb5b802853ac7
terry67867 <terry67867@naver.com>
2017-12-08 16:11:29 +0900

    갱신

--- practice1.rb
+++ practice1.rb
@@ -4,18 +4,18 @@ require 'httparty'
 #원하는 정보를 가져오기 위한 nokogiri
 require 'nokogiri'
 
-#1. 네이버 국내증시 주소를 url에 저장한다.
-url = "http://news.naver.com/main/read.nhn?mode=LSD&mid=shm&sid1=101&sid2=262&oid=421&aid=0003088684"
+#1. 블로그 주소를 url에 저장한다.
+url = "http://decode.tistory.com/"
 #2. url을 통해서 요청한다.
 response = HTTParty.get(url)
 #3. 가져온 문서를 nokogiri형식으로 저장
 doc = Nokogiri::HTML(response)
 #4. 문서에서 css중 ID가 KOSPI_now 인 것을 kospi 변수에 저장
 #css를 가져오기 위해서 크롬에서 copy selector
-kospi = doc.css("#articleBodyContents")
-puts kospi.class
-puts kospi.text.to_s
-puts kospi.text.include?"a"
+sentence = doc.css("#content")
+puts sentence.class
+#puts sentence.text.to_s
+puts sentence.text.include?"아두이노 나노"
 #article = kospi.split(" ")
 # puts article
 #article = kospi.include("제보")
